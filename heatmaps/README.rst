b436758051b4b8b5b29a8c8b48f0cc6dabc21a44
Rob Partington <zimpenfish@gmail.com>
2010-02-24 20:03:35 +0000

    - updated plot_heatmap.pl with general tidying up and niceness - added README

---
+++ heatmaps/README.rst
@@ -0,0 +1,18 @@
+Simple heatmaps
+===============
+
+Example
+-------
+
+::
+
+  perl heatmaps/plot_heatmap.pl --colourfile heatmaps/colors.png --latitude 53.45 --longitude -2.30 --zoom 11 --output /var/www/output.png gpsdata.csv
+
+Options
+-------
+--latitude  latitude of the centre point (52.5)
+--longitude  longitude of the centre point (-1.5)
+--zoom  zoom level according to google (7)
+--colourfile  heatmap palette (colors.png)
+--output  output filename (STDOUT if not given)
+--size  pixel size of the output (600x600, max 640x640)
--- heatmaps/plot_heatmap.pl
+++ heatmaps/plot_heatmap.pl
@@ -1,33 +1,93 @@
+#! /usr/bin/perl
+
 use Image::Magick;
+use Getopt::Long;
+use LWP::Simple;
 
-    my $image = Image::Magick->new();
-    $image->Set(size=>'600x600');
-    $image->Set(format=>'png');
-    $image->ReadImage('xc:black', '/home/rjp/colors.png');
-    my $radius = 1;
+my $size = '600x600';
+my $output = undef;
+my $colourfile = "colors.png";
 
-    my $max = -9999999999;
-    open F, $ARGV[0];
-    while (<F>) {
-        my ($date, $time, $lat, $long, $media) = split /,/;
-        my ($px, $py, $wpx, $wpy) = ll_to_px($lat, $long, 52.5, -1.5, 7, 600, 600);
-        my $points = sprintf("%d,%d %d,%d", $px, $py, $px+$radius, $py+$radius);
+# reasonable defaults for most of Britain
+my $centre_lat = '52.5';
+my $centre_long = '-1.5';
+my $zoom = 7;
+
+my $result = GetOptions(
+    "latitude=f" => \$centre_lat,
+    "longitude=f" => \$centre_long,
+    "zoom=i" => \$zoom,
+    "size=s" => \$size,
+    "output=s" => \$output,
+    "colourfile=s" => \$colourfile,
+);
+
+if ($size !~ /^(\d+)x(\d+)$/) {
+    die "Error: size must be {number}x{number}, you wanted $size";
+}
+
+my ($width, $height) = $size =~ /^(\d+)x(\d+)$/;
+if ($width > 640 or $height > 640) {
+    die "Error: size must be less than 640x640, you wanted $size";
+}
+
+if (! -r $colourfile) {
+    die "Error: $colourfile is not readable";
+}
+
+my $static_map = "http://maps.google.com/maps/api/staticmap?size=${size}&sensor=false&center=${centre_lat},${centre_long}&zoom=${zoom}";
+
+my $image = Image::Magick->new();
+$image->Set(size=>$size);
+$image->Set(format=>'png');
+$image->ReadImage('xc:black', $colourfile);
+my $radius = 1;
+
+# magic FH, STDIN or files on commandline
+while (<>) {
+    my ($lat, $long) = split /,/;
+    my ($px, $py, $wpx, $wpy) = ll_to_px($lat, $long, $centre_lat, $centre_long, $zoom, $width, $height);
+    my $points = sprintf("%d,%d %d,%d", $px, $py, $px+$radius, $py+$radius);
+    if ($px >= 0 and $px <= $width and $py >= 0 and $py <= $height) {
         $image->[0]->Draw(primitive=>'circle',stroke=>'none',fill=>'#CCCCCCF4',points=>$points);
-
-
-            my $colour = "#00000008";
-            $image->Draw(primitive=>'circle',fill=>$colour,stroke=>$colour,points=>"$px,$py ".($px+1).",".($py+1));
     }
-    close F;
+}
 
-    $image->[0]->ContrastStretch(channel => 'All', levels => '5%');
-    $image->[0]->Blur(radius => 2, sigma => 2);
-    $image->[0]->Set(type => 'TrueColorMatte');
-    my $p = $image->Fx(expression=>"v.p{0,u*v.h}");
+$image->[0]->ContrastStretch(channel => 'All', levels => '5%');
+$image->[0]->Blur(radius => 2, sigma => 2);
+$image->[0]->Set(type => 'TrueColorMatte');
+my $p = $image->Fx(expression=>"v.p{0,u*v.h}");
 
-    $| = 1;
-    binmode STDOUT;
-    $p->Write('png:-');
+$| = 1;
+binmode STDOUT;
+
+my $gmap = get($static_map);
+if (defined $gmap) { # we got our static map
+    use IO::String;
+    my $data = IO::String->new($gmap);
+    my $png = Image::Magick->new(magick=>'png');
+    my $x = $png->BlobToImage($gmap);
+    die "$x" if "$x";
+    $png->Composite(image => $p, compose => 'Multiply', gravity => 'center');
+    if (not defined $output) {
+        $png->Write('png:-');
+        print STDERR "combined heatmap/gmap written to STDOUT\n";
+    } else {
+        $png->Write($output);
+        print STDERR "combined heatmap/gmap written to $output\n";
+    }
+} else {
+    print STDERR "# couldn't fetch static gmap, try these commands:\n";
+    my $to = 'your_stdout.png';
+    if (not defined $output) {
+        $p->Write('png:-');
+    } else {
+        $p->Write($output);
+        $to = $output;
+    }
+    print STDERR "wget -O tmp.png '$static_map'\n";
+    print STDERR "composite -compose Multiply -gravity center $to tmp.png heatmap.png\n";
+}
 
 sub pi {
     return 3.1415926;
